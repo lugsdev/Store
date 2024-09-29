53b9e5745495ab6ce7a61c7d8441f2166b956522
Lucas Almeida <lucasags.dev@gmail.com>
2024-09-29 00:15:06 -0300

    CRUD simples para Livro finalizado

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -16,6 +16,32 @@ namespace StoreApi.Controllers
 			_livroRepository = livroRepository;
 		}
 
+		[HttpGet]
+		public IActionResult Get()
+		{
+			try
+			{
+				return Ok(_livroRepository.ObterTodos());
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e);
+			}
+		}
+
+		[HttpGet("{id:int}")]
+		public IActionResult Get([FromRoute]int id)
+		{
+			try
+			{
+				return Ok(_livroRepository.ObterPorIrd(id));
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e);
+			}
+		}
+
 		[HttpPost]
 		public IActionResult Post([FromBody] LivroInput input)
 		{
@@ -28,6 +54,38 @@ namespace StoreApi.Controllers
 				};
 				_livroRepository.Cadastrar(livro);
 
+				return Ok();
+			}
+			catch (Exception e) 
+			{ 
+				return BadRequest(e);
+			}
+		}
+
+		[HttpPut]
+		public IActionResult Put([FromBody] LivroUpdateInput input)
+		{
+			try
+			{
+				var livro = _livroRepository.ObterPorIrd(input.Id);
+				livro.Nome = input.Nome;
+				livro.Editora = input.Editora;
+
+				_livroRepository.Alterar(livro);
+				return Ok();
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e);
+			}
+		}
+
+		[HttpDelete("{id:int}")]
+		public IActionResult Delete([FromRoute] int id)
+		{
+			try
+			{
+				_livroRepository.Excluir(id);
 				return Ok();
 			}
 			catch (Exception e) 
