0ea451c265c46f49d1f0debb3c8d0daebe524d3e
Lucas Almeida <lucasags.dev@gmail.com>
2024-09-29 00:31:37 -0300

    Implementação da Controller Cliente com Método Get do CRUD

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,31 @@
+﻿using Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace StoreApi.Controllers
+{
+	[ApiController]
+	[Route("/[controller]")]
+	public class ClienteController : ControllerBase
+	{
+		private readonly IClienteRepository _clienteRepository;
+
+		public ClienteController(IClienteRepository clienteRepository)
+		{
+			_clienteRepository = clienteRepository;
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			try
+			{
+				return Ok(_clienteRepository.ObterTodos());
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e);
+			}
+		}
+	}
+}
